cf74db184cdf5d0029eb81c7e18e04d527b74277
ZeeShan <m.zeeshan1600@gmail.com>
2022-10-06 18:06:54 +0500

    HTML Fillint the Templates:

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ const url = require('url')
 // Routing: mean implementing different actions for different URLs.
 
 // fs.readFile("./dev-data/data.json")
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObject = JSON.parse(data)
 
@@ -26,15 +43,26 @@ const dataObject = JSON.parse(data)
 const server = http.createServer((req, res) => {
     console.log(req.url)
     const pathName = req.url
+
+    // Overview Page
     if (pathName === '/' || pathName === '/overview') {
-        res.end("This is a Overview")
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        const cardHtml = dataObject.map(element => replaceTemplate(tempCard, element)).join('')
+        const output = tempOverview.replace('{%PRODUCTNAME%}', cardHtml)
+        res.end(output)
+        // res.end("This is a Overview")
+
+        // Product Page
     } else if (pathName === '/product') {
         res.end("This is a Product Page")
+
+        // API  
     } else if (pathName === '/api') {
         // Scenario: want to read data from the JSON data file, then parson JSON to JS, and then send back to the client
         res.writeHead(200, { 'Content-type': 'application.json' });
         res.end(dataObject)
 
+        // Not Found
     } else {
         // we can send something more in writeHead that's header => to specify header we need an object
         // Header: http header basically piece of information about the response that we are sending back
